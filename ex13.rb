9a41ac7312f6e98d5c79c0ddb4ccde1658882d4f
makanazilla <36710772+makanazilla@users.noreply.github.com>
2019-10-12 13:19:08 -0700

    update for user input

--- ex13.rb
+++ ex13.rb
@@ -1,5 +1,14 @@
 first, second, third = ARGV
 
+puts "What's your first variable?"
+first = $stdin.gets.chomp
+
+puts "What's your second variable?"
+second = $stdin.gets.chomp
+
+puts "What's your third variable?"
+third = $stdin.gets.chomp
+
 puts "Your first variable is: #{first}"
 
 puts "Your second variable is: #{second}"
