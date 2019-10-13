a044741735ff1d3dea5fb6615a1941b3cbbb2a41
makanazilla <36710772+makanazilla@users.noreply.github.com>
2019-10-12 18:14:59 -0700

    Initial commit

--- ex15.rb
+++ ex15.rb
@@ -1,1 +1,13 @@
+filename = ARGV.first
 
+txt = open(filename)
+
+puts "Here's your file #{filename}:"
+print txt.read
+
+print "Typel the file name again: "
+file_again = $stdin.gets.chomp
+
+txt_again = open(file_again)
+
+print txt_again.read
