783b0c9bacfd106b851af2048efa0d366bbeeb11
makanazilla <36710772+makanazilla@users.noreply.github.com>
2019-10-12 18:15:15 -0700

    fix spelling error

--- ex15.rb
+++ ex15.rb
@@ -5,7 +5,7 @@ txt = open(filename)
 puts "Here's your file #{filename}:"
 print txt.read
 
-print "Typel the file name again: "
+print "Type the file name again: "
 file_again = $stdin.gets.chomp
 
 txt_again = open(file_again)
