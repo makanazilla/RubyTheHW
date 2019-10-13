2a9cc447d3335bb8febdc39d773af14b4e524350
makanazilla <36710772+makanazilla@users.noreply.github.com>
2019-10-12 18:21:33 -0700

    Study drill 4
    Get rid of the lines 8-13 where you use gets.chomp and run the script again.

--- ex15.rb
+++ ex15.rb
@@ -9,11 +9,11 @@ puts "Here's your file #{filename}:"
 print txt.read
 
 #Takes in user input asking you to print out the file name
-print "Type the file name again: "
-file_again = $stdin.gets.chomp
+#print "Type the file name again: "
+#file_again = $stdin.gets.chomp
 
 #Sets txt_again to open the file name that was input
-txt_again = open(file_again)
+#txt_again = open(file_again)
 
 #prints out the file that was passed in above
-print txt_again.read
+#print txt_again.read
