38cd3a7738be244c4e72286cf1d8cbc3c7d6b79d
makanazilla <36710772+makanazilla@users.noreply.github.com>
2019-10-12 18:19:08 -0700

    Study Drill 1
    Above each line, write out in English what that line does.

--- ex15.rb
+++ ex15.rb
@@ -1,13 +1,19 @@
+#Sets file name to take the first argument in the command line
 filename = ARGV.first
 
+#Sets txt to open whichever filename is passed in
 txt = open(filename)
 
+#Writes out the name of the file that it is printing out and prints out the content of the file
 puts "Here's your file #{filename}:"
 print txt.read
 
+#Takes in user input asking you to print out the file name
 print "Type the file name again: "
 file_again = $stdin.gets.chomp
 
+#Sets txt_again to open the file name that was input
 txt_again = open(file_again)
 
+#prints out the file that was passed in above
 print txt_again.read
